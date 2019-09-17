e75eaca130f8e38a27a35675f3dcae9bd2933fac
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-09-17 17:35:39 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,18 @@
-variable "aws_access_key" {
-}
+variable "aws_access_key" {}
 
-variable "aws_secret_key" {
-}
+variable "aws_secret_key" {}
 
 variable "aws_region" {
   default = "us-east-1"
 }
 
 variable "client_cidr_block" {
+  description = "The IPv4 address range, in CIDR notation being /22 or greater, from which to assign client IP addresses"
   default = "18.0.0.0/22"
 }
 
-variable "subnet-id" {
+variable "subnet_id" {
+  description = "The ID of the subnet to associate with the Client VPN endpoint."
 }
 
 variable "cert_dir" {
