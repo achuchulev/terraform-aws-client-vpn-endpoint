2b33c256e68d19201645ffcb6fd5c394d60927b7
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-09-19 15:13:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ resource "aws_acm_certificate" "server_cert" {
 
 resource "aws_ec2_client_vpn_endpoint" "client-vpn-endpoint" {
   description            = "terraform-clientvpn-endpoint"
-  server_certificate_arn = aws_acm_certificate.server_cert.arn
+  server_certificate_arn = "arn:aws:acm:us-east-1:618541241448:certificate/cc679b81-ea37-4032-a3ee-901061de0d52" #aws_acm_certificate.server_cert.arn
   client_cidr_block      = var.client_cidr_block
 
   authentication_options {
     type                       = "certificate-authentication"
-    root_certificate_chain_arn = aws_acm_certificate.client_cert.arn
+    root_certificate_chain_arn = "arn:aws:acm:us-east-1:618541241448:certificate/5f9e4b96-843c-4717-bcee-82031c681305" #aws_acm_certificate.client_cert.arn
   }
 
   connection_log_options {
@@ -49,7 +49,10 @@ resource "null_resource" "authorize-client-vpn-ingress" {
     command = "aws --region ${var.aws_region} ec2 authorize-client-vpn-ingress --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --target-network-cidr 0.0.0.0/0 --authorize-all-groups"
   }
 
-  depends_on = [aws_ec2_client_vpn_endpoint.client-vpn-endpoint]
+  depends_on = [
+    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
+    aws_ec2_client_vpn_network_association.client-vpn-network-association
+  ]
 }
 
 resource "null_resource" "create-client-vpn-route" {
@@ -57,7 +60,10 @@ resource "null_resource" "create-client-vpn-route" {
     command = "aws --region ${var.aws_region} ec2 create-client-vpn-route --client-vpn-endpoint-id ${aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id} --destination-cidr-block 0.0.0.0/0 --target-vpc-subnet-id ${var.subnet_id} --description Internet-Access"
   }
 
-  depends_on = [aws_ec2_client_vpn_endpoint.client-vpn-endpoint]
+  depends_on = [
+    aws_ec2_client_vpn_endpoint.client-vpn-endpoint,
+    null_resource.authorize-client-vpn-ingress
+  ]
 }
 
 resource "null_resource" "export-client-config" {
@@ -80,4 +86,3 @@ resource "null_resource" "append-client-config-certs" {
 
   depends_on = [null_resource.export-client-config]
 }
-
